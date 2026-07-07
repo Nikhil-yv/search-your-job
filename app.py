acfe1f9097bc6a7a38d2d7386b47d321ee510f7c
Nikhil-yv <nikhil.elpula01@gmail.com>
2026-07-07 16:53:29 -0400

    added few more portals

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ if st.button("Search"):
         with st.spinner('Searching...'):
 
             jobs = scrape_jobs(
-                site_name=["indeed", "linkedin", "glassdoor"],
+                site_name=["indeed", "linkedin", "glassdoor", "google", "zip_recruiter"],
                 search_term=search_keyword,
                 location=location,
                 results_wanted=20,
