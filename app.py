23a16c1b88259dff13bb2028dc8fae144e4161d8
Nikhil-yv <nikhil.elpula01@gmail.com>
2026-07-07 17:24:33 -0400

    fix syntax

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ if st.button("Search"):
                 results_wanted=20,
                 hours_old=72
             )
-cols = ['title'] + [c for c in jobs.columns if c not in ['title', 'id']] + ['id']            jobs = jobs[cols]
+            cols = ['title'] + [c for c in jobs.columns if c not in ['title', 'id']] + ['id']            
+            jobs = jobs[cols]
             st.success(f"Found {len(jobs)} jobs!")
             st.dataframe(
             jobs,
