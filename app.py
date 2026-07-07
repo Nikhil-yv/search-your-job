26f3cf4684b9e2102db350b859a311a715817cb5
Nikhil-yv <nikhil.elpula01@gmail.com>
2026-07-07 16:40:51 -0400

    added two portals

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from jobspy import scrape_jobs
 import pandas as pd
 
-# Use Streamlit widgets instead of input()
 st.title("Job Search Tool")
 search_keyword = st.text_input("Enter Job Title:")
 location = st.text_input("Enter Location:")
@@ -10,9 +9,9 @@ location = st.text_input("Enter Location:")
 if st.button("Search"):
     if search_keyword and location:
         with st.spinner('Searching...'):
-            # Call your scraping function
+
             jobs = scrape_jobs(
-                site_name=["indeed"],
+                site_name=["dice", "indeed"],
                 search_term=search_keyword,
                 location=location,
                 results_wanted=20,
@@ -21,7 +20,6 @@ if st.button("Search"):
             st.success(f"Found {len(jobs)} jobs!")
             st.dataframe(jobs) # Display results in the app
             
-            # Optional: Provide a download button
             csv = jobs.to_csv(index=False).encode('utf-8')
             st.download_button("Download CSV", csv, "jobs.csv", "text/csv")
     else:
