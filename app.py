09bc5d5e5632d4cf20048cb614bbfe954c3c9f5c
Nikhil-yv <nikhil.elpula01@gmail.com>
2026-07-07 16:48:56 -0400

    Removed zip recruiter

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ if st.button("Search"):
         with st.spinner('Searching...'):
 
             jobs = scrape_jobs(
-                site_name=["indeed", "linkedin", "glassdoor", "monster", "ziprecruiter"],
+                site_name=["indeed", "linkedin", "glassdoor", "monster"],
                 search_term=search_keyword,
                 location=location,
                 results_wanted=20,
